0d833e472367ff9d6dfd1f07e5223e63dcfed92d
Shevilll <ahmadfaraz00710@gmail.com>
2024-12-18 18:21:23 +0530

    .

--- quizinx/settings.py
+++ quizinx/settings.py
@@ -27,7 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:5173", "http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "https://quizinx.vercel.app/",
+]
 
 # Application definition
 
